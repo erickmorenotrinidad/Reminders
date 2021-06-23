7ac2dac76bb7bc82cf2f435f90f6cbf41c857d23
erickmorenotrinidad <86013989+erickmorenotrinidad@users.noreply.github.com>
2021-06-23 11:12:39 -0700

    Update main.py
    Added options for users to pick

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+from reminders import Reminders
+reminders_list = []
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    while True:
+        option = input('1. New '
+                      '2. delete')
+        print(reminders_list)
+        if option == '1':
+            first = Reminders()
+            first.name()
+            reminders_list.append(first)
