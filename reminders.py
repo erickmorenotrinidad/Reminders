1cd6849a53da0f7d04924113490e1bb3b77b70d4
erickmorenotrinidad <86013989+erickmorenotrinidad@users.noreply.github.com>
2021-06-23 11:13:12 -0700

    Update reminders.py
    we added methods for each option

--- reminders.py
+++ reminders.py
@@ -1,4 +1,31 @@
 class Reminders:
     def __init__(self):
         pass
-    
+
+    def name(self):
+        input('What is the name for the reminder?')
+
+    def set_date(self, date):
+        self.date = date
+
+    def set_time(self, time):
+        self.time = time
+
+    def set_tag(self, tag):
+        self.tag = tag
+
+    def set_notes(self, notes):
+        self.notes = notes
+
+    def set_priority(self, priority):
+        self.priority = priority
+
+    def set_complete(self, complete):
+        self.complete = complete
+
+    def set_location(self, location):
+        self.location = location
+
+    def repeat(self):
+        pass
+
